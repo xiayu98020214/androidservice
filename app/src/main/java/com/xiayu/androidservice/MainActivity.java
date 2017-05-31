4a23a758c479c919c5a23deb7aff03835fa2966e
yu.xia <yu.xia@zhongtoutiandi.com>
2017-05-31 17:36:00 +0800

    add aidl service

--- app/src/main/java/com/xiayu/androidservice/MainActivity.java
+++ app/src/main/java/com/xiayu/androidservice/MainActivity.java
@@ -1,20 +1,28 @@
 package com.xiayu.androidservice;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private Button startService;
     private Button stopService;
     private Button bindService;
     private Button unbindService;
+    private Button aidlService;
 
     private MyService.DownloadBinder downloadBinder;
     private ServiceConnection connection = new ServiceConnection() {
@@ -42,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         stopService = (Button)findViewById(R.id.stop_service);
         bindService = (Button)findViewById(R.id.bind_service);
         unbindService = (Button)findViewById(R.id.unbind_service);
+        aidlService = (Button)findViewById(R.id.aidl_service);
 
         startService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +82,85 @@ public class MainActivity extends AppCompatActivity {
                 unbindService(connection);
             }
         });
+        aidlService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptToBindService();
+                addBook();
+            }
+        });
 
+    }
+
+    private BookManager mBookManager = null;
+    private boolean mBound = false;
+
+    //包含Book对象的list
+    private List<Book> mBooks;
+
+    public void addBook() {
+        //如果与服务端的连接处于未连接状态，则尝试连接
+        if (!mBound) {
+            attemptToBindService();
+            Toast.makeText(this, "当前与服务端处于未连接状态，正在尝试重连，请稍后再试", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mBookManager == null) return;
+
+        Book book = new Book();
+        book.setName("APP研发录In");
+        book.setPrice(30);
+        try {
+            mBookManager.addBook(book);
+            List<Book> books = mBookManager.getBooks();
+            Log.e(TAG,"books:"+books);
+            Log.e(getLocalClassName(), book.toString());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+
+    /**
+     * 尝试与服务端建立连接
+     */
+    private void attemptToBindService() {
+        Intent intent = new Intent();
+        intent.setAction("com.xiayu.aidl");
+        intent.setPackage("com.xiayu.androidservice");
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.e(getLocalClassName(), "service connected");
+            mBookManager = BookManager.Stub.asInterface(service);
+            mBound = true;
+
+            if (mBookManager != null) {
+                try {
+                    mBooks = mBookManager.getBooks();
+                    Log.e(getLocalClassName(), mBooks.toString());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.e(getLocalClassName(), "service disconnected");
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBound) {
+            attemptToBindService();
+        }
     }
 }
